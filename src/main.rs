1e8c117fd9ff9284796f3a318d5b5973632a4edf
mat <github@matdoes.dev>
2022-05-23 13:07:40 -0500

    reqwest compression

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,18 @@ pub mod mojang;
 pub mod render;
 
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use std::io::Cursor;
+use std::{io::Cursor, time::Instant};
 
 #[get("/")]
 async fn index() -> impl Responder {
     HttpResponse::Ok()
         .body(
-            "mat's super duper simple Minecraft head renderer\n\n\
+            "mat's super duper simple minecraft head renderer\n\n\
             Usage:\n\
               /2d/<id>.png - returns an 8x8 image of the front of the player's Minecraft head\n\
               /3d/<id>.png - returns a 128x128 image of the player's Minecraft head, the same way it'd look in a Minecraft inventory\n\n\
             You can use either an undashed player UUID or a resource ID.\n\n\
-            https://github.com/mat-1/ TODO: add link to repo"
+            https://github.com/mat-1/msdsmchr"
         )
 }
 
@@ -64,6 +64,7 @@ async fn make_3d_head(id: web::Path<String>) -> impl Responder {
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     println!("Running :)");
+
     HttpServer::new(|| {
         App::new()
             .service(index)
@@ -78,15 +79,12 @@ async fn main() -> std::io::Result<()> {
 // fn main() {
 //     let skin = image::open("skin.png").unwrap();
 
-//     // for _ in 0..1000 {
-//     let skin_image = render::to_3d_head(&skin);
-//     let mut buf = Cursor::new(Vec::new());
-//     skin_image
-//         .write_to(&mut buf, image::ImageOutputFormat::Png)
-//         .unwrap();
-//     // let head =
-//     //     oxipng::optimize_from_memory(&buf.into_inner(), &oxipng::Options::from_preset(0))
-//     //         .unwrap();
-//     // }
-//     skin_image.save("head.png").unwrap();
+//     for _ in 0..1000 {
+//         let skin_image = render::to_3d_head(&skin);
+//         let mut buf = Cursor::new(Vec::new());
+//         skin_image
+//             .write_to(&mut buf, image::ImageOutputFormat::Png)
+//             .unwrap();
+//     }
+//     // skin_image.save("head.png").unwrap();
 // }
